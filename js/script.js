22a4a8e09dcd7e01f80bd574208824cc567c9b22
Sumit Das <sumit@example.com>
2026-03-06 13:42:40 +0600

    add

--- js/script.js
+++ js/script.js
@@ -15,3 +15,27 @@ jQuery(document).ready(function($) {
     //...
   });
 });
+
+
+let counterUp=document.querySelectorAll(".counter")
+
+let arr=Array.from(counterUp)
+
+arr.map((item)=>{
+let counter=0;
+
+  function count(){
+    counter++;
+    item.innerHTML=counter
+    if(counter==item.dataset.number){
+clearInterval(stop)
+    }
+
+  }
+
+  let stop=setInterval(function(){
+    count();
+  },1000/item.dataset.number)
+
+})
+
